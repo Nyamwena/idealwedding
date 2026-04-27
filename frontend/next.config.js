02277f6a73b091a33e0d3a9fa47b337fbde4e341
iamroot <nyamselis@gmail.com>
2026-04-27 14:27:54 +0200

    logo

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,22 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
-    NEXT_PUBLIC_AUTH_SERVICE_URL: process.env.NEXT_PUBLIC_AUTH_SERVICE_URL || 'http://localhost:3002',
-  },
+    images: {
+        unoptimized: true, // 🔥 fixes ECONNREFUSED issue
+    },
+    env: {
+        NEXT_PUBLIC_API_URL:
+            process.env.NEXT_PUBLIC_API_URL || 'https://idealweddings.space/',
+        NEXT_PUBLIC_AUTH_SERVICE_URL:
+            process.env.NEXT_PUBLIC_AUTH_SERVICE_URL ||
+            'https://api-gateway.idealweddings.space',
+    },
 };
 
 module.exports = withPWA(nextConfig);
